b169de9e267e2263a873c7fa27b58202fc988b0e
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-11 11:55:56 +0900

    [FIX] Swagger 오류 해결

--- src/main/java/sparta/team6/momo/security/oauth/OAuth2SuccessHandler.java
+++ src/main/java/sparta/team6/momo/security/oauth/OAuth2SuccessHandler.java
@@ -1,11 +1,13 @@
 package sparta.team6.momo.security.oauth;
 
-import antlr.Token;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import sparta.team6.momo.security.auth.PrincipalDetails;
+import sparta.team6.momo.dto.Success;
+import sparta.team6.momo.dto.TokenDto;
 import sparta.team6.momo.security.jwt.JwtFilter;
 import sparta.team6.momo.security.jwt.TokenProvider;
 
@@ -16,16 +18,32 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final TokenProvider tokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         String jwt = tokenProvider.createToken(authentication);
+        setResponseWithJwt(response, jwt);
+    }
+
+    private void setResponseWithJwt(HttpServletResponse response, String jwt) throws IOException {
+        setHeader(response, jwt);
+        setBody(response, jwt);
+    }
+
+    private void setHeader(HttpServletResponse response, String jwt) {
+        response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType("text/html;charset=UTF-8");
         response.addHeader(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+    }
+
+    private void setBody(HttpServletResponse response, String jwt) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(jwt);
+        Success<TokenDto> success = Success.tokenDtoSuccess(jwt);
+        response.getWriter().write(objectMapper.writeValueAsString(success));
     }
 }
