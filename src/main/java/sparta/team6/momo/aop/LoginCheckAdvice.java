fcaf180f7433755d8d20d5cabbaf681a77677b1a
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-15 22:09:06 +0900

    save

--- src/main/java/sparta/team6/momo/aop/LoginCheckAdvice.java
+++ src/main/java/sparta/team6/momo/aop/LoginCheckAdvice.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import sparta.team6.momo.exception.CustomException;
-import sparta.team6.momo.exception.ErrorCode;
 
 import static sparta.team6.momo.exception.ErrorCode.ONLY_LOGOUT_ACCESS;
 
