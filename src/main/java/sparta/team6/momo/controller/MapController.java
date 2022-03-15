1cd838698adb5f82bdf1f9f46332a28be27f7e99
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-15 16:08:14 +0900

    [TEST] SOCKET TEST

--- src/main/java/sparta/team6/momo/controller/MapController.java
+++ src/main/java/sparta/team6/momo/controller/MapController.java
@@ -3,6 +3,7 @@ package sparta.team6.momo.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import sparta.team6.momo.dto.LocationDto;
 
@@ -11,8 +12,21 @@ public class MapController {
 
     @MessageMapping("/")
     @SendTo("/topic/public")
-    public LocationDto sendMessage(@Payload LocationDto locationDto) {
+    public LocationDto sendLocation(@Payload LocationDto locationDto) {
         return locationDto;
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public LocationDto sendMessage(@Payload LocationDto chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public LocationDto addUser(@Payload LocationDto chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getTitle());
+        return chatMessage;
     }
 }
 
