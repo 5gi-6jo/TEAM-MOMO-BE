bbf499e8467bdebe6570a47a3719d2c123f39ec5
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-16 17:37:07 +0900

    save

--- src/main/java/sparta/team6/momo/security/config/WebSecurityConfig.java
+++ src/main/java/sparta/team6/momo/security/config/WebSecurityConfig.java
@@ -65,7 +65,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/images/**").permitAll()
                 .antMatchers("/css/**").permitAll()
