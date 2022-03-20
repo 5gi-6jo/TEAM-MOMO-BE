3d2fcfacc9526dd4f318572ffd77fec5172e4218
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-20 19:41:56 +0900

    [feat] jwt error

--- src/main/java/sparta/team6/momo/security/jwt/TokenProvider.java
+++ src/main/java/sparta/team6/momo/security/jwt/TokenProvider.java
@@ -3,10 +3,12 @@ package sparta.team6.momo.security.jwt;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,6 +16,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import sparta.team6.momo.dto.TokenDto;
+import sparta.team6.momo.exception.DefaultException;
 import sparta.team6.momo.repository.UserRepository;
 import sparta.team6.momo.security.auth.MoMoUser;
 
@@ -75,16 +78,15 @@ public class TokenProvider implements InitializingBean {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("잘못된 JWT 서명입니다.");
+        } catch (SecurityException | MalformedJwtException e) {
+            throw new DefaultException(HttpStatus.BAD_REQUEST, "잘못 구성된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
-            log.info("만료된 JWT 토큰입니다.");
+            throw new DefaultException(HttpStatus.BAD_REQUEST, "만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
-            log.info("지원되지 않는 JWT 토큰입니다.");
+            throw new DefaultException(HttpStatus.BAD_REQUEST, "지원되지 않는 형식의 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            log.info("JWT 토큰이 잘못되었습니다.");
+            throw new DefaultException(HttpStatus.BAD_REQUEST, "JWT 토큰이 잘못되었습니다.");
         }
-        return false;
     }
 
     public Long getRemainExpiration(String token) {
