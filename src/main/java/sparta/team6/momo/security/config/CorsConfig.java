68d314d8dbfd82f8cba0d809a558ccb142f7e793
HoduUlmu <62408879+HoduUlmu@users.noreply.github.com>
2022-03-21 22:25:28 +0900

    [fix] url

--- src/main/java/sparta/team6/momo/security/config/CorsConfig.java
+++ src/main/java/sparta/team6/momo/security/config/CorsConfig.java
@@ -16,12 +16,15 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
         config.addExposedHeader("Set-Cookie");
 
         source.registerCorsConfiguration("/users/**", config);
+        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/kakao/**", config);
         source.registerCorsConfiguration("/plans/**", config);
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
